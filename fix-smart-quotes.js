bdd1caa410961131291e1b2f973d1f8fd1b34e5b
Khalil Ez-zahid <khalilezzahid03@gmail.com>
2026-08-15 01:19:09 +0100

    Fix syntax errors in ChatArea.tsx - fixed JSX closing tags and smart quote issues

--- fix-smart-quotes.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const filePath = 'C:\\Users\\Lylo\\Documents\\petroshield-chat\\client\\src\\components\\ChatArea.tsx';
-const content = fs.readFileSync(filePath, 'utf8');
-
-// Replace smart quotes (the garbled pattern)
-const fixed = content.replace(/A��'A,A�A�A�A��,���A,A�A�A�A��\?sA�A,A\?/g, '"');
-fs.writeFileSync(filePath, fixed, 'utf8');
-console.log('Done fixing smart quotes');
